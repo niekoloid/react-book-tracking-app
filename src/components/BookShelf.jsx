ada89d24eb3eb8088f879f2c78d2ec8279cbe6b6
sunsagong <sagong.sun@gmail.com>
2018-04-02 17:33:07 +0800

    take a break

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
-import BookList from './BookList'
 
 class BookShelf extends React.Component {
   render() {
@@ -11,8 +9,25 @@ class BookShelf extends React.Component {
     return(
       <div className="bookshelf">
       <h2 className="bookshelf-title">{titleShelf}</h2>
-      <ol>
-        {books.map(book => (<li key={book.title}>{book.title}</li>))}
+      <ol className="books-grid">
+        {books.map(book => (<li key={book.title}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 200, backgroundImage: book.backgroundImage }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.author}</div>
+          </div>
+        </li>))}
       </ol>
       </div>
     )
